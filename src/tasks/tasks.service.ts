2145ffd33b457dd36929295c479d7d40e77a03a1
surendrakanigiri <surendra@krify.net>
2024-03-11 08:50:42 +0530

    Did create task API

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
-import { type Task } from './task.model'
+import { TaskStatus, type Task } from './task.model'
+import * as uuid from 'uuid'
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,17 @@ export class TasksService {
 
   getAllTasks (): Task[] {
     return this.tasks
+  }
+
+  createTask (title: string, description: string): Task {
+    const task: Task = {
+      id: uuid.v4(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    }
+
+    this.tasks.push(task)
+    return task
   }
 }
