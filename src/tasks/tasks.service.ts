114f92dbcb2e2df4b20f1bffc8966926def303bc
surendrakanigiri <surendra@krify.net>
2024-03-12 09:16:52 +0530

    Implemented DTO to avoid static define of variables types

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { TaskStatus, type Task } from './task.model'
 import * as uuid from 'uuid'
+import { type CreateTaskDto } from './dto/create-task.dto'
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,8 @@ export class TasksService {
     return this.tasks
   }
 
-  createTask (title: string, description: string): Task {
+  createTask (createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto
     const task: Task = {
       id: uuid.v4(),
       title,
