360009c4ff8cd7c243313e51ae11d10483bbee71
surendrakanigiri <surendra@krify.net>
2024-03-14 09:24:30 +0530

    Commented old code

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common'
 import { TasksService } from './tasks.service'
-import { TaskStatus, type Task } from './task.model'
 import { CreateTaskDto } from './dto/create-task.dto'
 import { GetTasksByFilterDto } from './dto/get-tasks-by-filter.dto'
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe'
@@ -9,32 +8,32 @@ import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe'
 export class TasksController {
   constructor (private readonly tasksService: TasksService) {}
 
-  @Get()
-  getAllTasks (@Query() fileterData: GetTasksByFilterDto): Task[] | undefined {
-    if (Object.keys(fileterData).length > 0) {
-      return this.tasksService.getTasksByFilter(fileterData)
-    }
-    return this.tasksService.getAllTasks()
-  }
+  // @Get()
+  // getAllTasks (@Query() fileterData: GetTasksByFilterDto): Task[] | undefined {
+  //   if (Object.keys(fileterData).length > 0) {
+  //     return this.tasksService.getTasksByFilter(fileterData)
+  //   }
+  //   return this.tasksService.getAllTasks()
+  // }
 
-  @Get('/:id')
-  getTaskById (@Param('id') id: string): Task | undefined {
-    return this.tasksService.getTaskById(id)
-  }
+  // @Get('/:id')
+  // getTaskById (@Param('id') id: string): Task | undefined {
+  //   return this.tasksService.getTaskById(id)
+  // }
 
-  @Post()
-  @UsePipes(ValidationPipe)
-  createTask (@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto)
-  }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createTask (@Body() createTaskDto: CreateTaskDto): Task {
+  //   return this.tasksService.createTask(createTaskDto)
+  // }
 
-  @Patch('/:id')
-  updateTaskById (@Param('id') id: string, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task | undefined {
-    return this.tasksService.updateTaskById(id, status)
-  }
+  // @Patch('/:id')
+  // updateTaskById (@Param('id') id: string, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task | undefined {
+  //   return this.tasksService.updateTaskById(id, status)
+  // }
 
-  @Delete('/:id')
-  deleteTaskById (@Param('id') id: string): void {
-    this.tasksService.deleteATask(id)
-  }
+  // @Delete('/:id')
+  // deleteTaskById (@Param('id') id: string): void {
+  //   this.tasksService.deleteATask(id)
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,65 +1,64 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
-import { TaskStatus, type Task } from './task.model'
-import * as uuid from 'uuid'
+import { TaskStatus } from './task.enum'
 import { type CreateTaskDto } from './dto/create-task.dto'
 import { type GetTasksByFilterDto } from './dto/get-tasks-by-filter.dto'
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = []
+  // private tasks: Task[] = []
 
-  getAllTasks (): Task[] {
-    return this.tasks
-  }
+  // getAllTasks (): Task[] {
+  //   return this.tasks
+  // }
 
-  getTasksByFilter (fileterData: GetTasksByFilterDto): Task[] | undefined {
-    const { search, status } = fileterData
-    let tasks = this.getAllTasks()
+  // getTasksByFilter (fileterData: GetTasksByFilterDto): Task[] | undefined {
+  //   const { search, status } = fileterData
+  //   let tasks = this.getAllTasks()
 
-    if (status !== null) {
-      tasks = tasks.filter(task => task.status === status)
-    }
+  //   if (status !== null) {
+  //     tasks = tasks.filter(task => task.status === status)
+  //   }
 
-    if (search !== null) {
-      tasks = tasks.filter(task =>
-        task.title.includes(search) ||
-        task.description.includes(search)
-      )
-    }
+  //   if (search !== null) {
+  //     tasks = tasks.filter(task =>
+  //       task.title.includes(search) ||
+  //       task.description.includes(search)
+  //     )
+  //   }
 
-    return tasks
-  }
+  //   return tasks
+  // }
 
-  getTaskById (id: string): Task | undefined {
-    const taskFound = this.tasks.find(task => task.id === id)
+  // getTaskById (id: string): Task | undefined {
+  //   const taskFound = this.tasks.find(task => task.id === id)
 
-    if (taskFound === undefined) {
-      throw new NotFoundException(`Task with id ${id} not found`)
-    }
+  //   if (taskFound === undefined) {
+  //     throw new NotFoundException(`Task with id ${id} not found`)
+  //   }
 
-    return taskFound
-  }
+  //   return taskFound
+  // }
 
-  updateTaskById (id: string, status: TaskStatus): Task | undefined {
-    const task = this.getTaskById(id)
-    if ((task?.status) != null) task.status = status
-    return task
-  }
+  // updateTaskById (id: string, status: TaskStatus): Task | undefined {
+  //   const task = this.getTaskById(id)
+  //   if ((task?.status) != null) task.status = status
+  //   return task
+  // }
 
-  createTask (createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto
-    const task: Task = {
-      id: uuid.v4(),
-      title,
-      description,
-      status: TaskStatus.OPEN
-    }
+  // createTask (createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto
+  //   const task: Task = {
+  //     id: uuid.v4(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN
+  //   }
 
-    this.tasks.push(task)
-    return task
-  }
+  //   this.tasks.push(task)
+  //   return task
+  // }
 
-  deleteATask (id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id)
-  }
+  // deleteATask (id: string): void {
+  //   this.tasks = this.tasks.filter(task => task.id !== id)
+  // }
 }
